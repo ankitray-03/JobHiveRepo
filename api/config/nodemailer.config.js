b8d2753e3c78147e5789d0646fade61431744cf4
ankit <ankitray0308@gmail.com>
2026-01-30 17:00:09 +0530

    setting limit

--- api/config/nodemailer.config.js
+++ api/config/nodemailer.config.js
@@ -18,20 +18,13 @@ const transporter = nodemailer.createTransport({
   secure: false, // TLS
   auth: {
     user: process.env.NODEMAILER_EMAIL,
-    pass: process.env.NODEMAILER_PASSWORD, // App password
+    pass: process.env.NODEMAILER_PASSWORD, // App Password
   },
   requireTLS: true,
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
 });
-
-// const transporter = nodemailer.createTransport({
-//   host: "smtp.gmail.com",
-//   port: 465,
-//   secure: true, // true for port 465, false for other ports
-//   auth: {
-//     user: process.env.NODEMAILER_EMAIL,
-//     pass: process.env.NODEMAILER_PASSWORD,
-//   },
-// });
 
 // const transporter = nodemailer.createTransport({
 //   host: "smtp-relay.brevo.com",

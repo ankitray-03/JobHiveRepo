72fbe7ad2efd6f96c20c5c0b2ef2dbc988231f83
ankit <ankitray0308@gmail.com>
2026-01-30 15:42:58 +0530

    Fixing the config file of nodemailer

--- api/config/nodemailer.config.js
+++ api/config/nodemailer.config.js
@@ -2,14 +2,25 @@ import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 dotenv.config();
 
+// const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   port: 465,
+//   secure: true, // true for port 465, false for other ports
+//   auth: {
+//     user: process.env.NODEMAILER_EMAIL,
+//     pass: process.env.NODEMAILER_PASSWORD,
+//   },
+// });
+
 const transporter = nodemailer.createTransport({
-  service: "gmail",
-  port: 465,
-  secure: true, // true for port 465, false for other ports
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // TLS
   auth: {
     user: process.env.NODEMAILER_EMAIL,
     pass: process.env.NODEMAILER_PASSWORD,
   },
+  connectionTimeout: 60000,
 });
 
 export default transporter;

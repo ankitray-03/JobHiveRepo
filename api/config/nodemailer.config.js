e8dd99837dfbb901e767c2942c5fdf135aea120f
ankit <ankitray0308@gmail.com>
2026-01-30 16:50:58 +0530

    editing port

--- api/config/nodemailer.config.js
+++ api/config/nodemailer.config.js
@@ -14,13 +14,24 @@ dotenv.config();
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true, // true for port 465, false for other ports
+  port: 587,
+  secure: false, // TLS
   auth: {
     user: process.env.NODEMAILER_EMAIL,
-    pass: process.env.NODEMAILER_PASSWORD,
+    pass: process.env.NODEMAILER_PASSWORD, // App password
   },
+  requireTLS: true,
 });
+
+// const transporter = nodemailer.createTransport({
+//   host: "smtp.gmail.com",
+//   port: 465,
+//   secure: true, // true for port 465, false for other ports
+//   auth: {
+//     user: process.env.NODEMAILER_EMAIL,
+//     pass: process.env.NODEMAILER_PASSWORD,
+//   },
+// });
 
 // const transporter = nodemailer.createTransport({
 //   host: "smtp-relay.brevo.com",
